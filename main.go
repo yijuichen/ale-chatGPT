b20e24779c1c530b54eadb48d0f4d832987d5468
ray-mac <ray-mac@ray-macdeMacBook-Air.local>
2024-08-25 12:38:27 +0800

    test

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	openai "github.com/sashabaranov/go-openai"
+	"github.com/spf13/cobra"
+)
+
+func GetResponse(client *openai.Client, ctx context.Context, promptStr string) {
+	req := openai.CompletionRequest{
+		Model:     openai.GPT4oMini,
+		MaxTokens: 300,
+		Prompt:    promptStr,
+		Stream:    true,
+	}
+
+	resp, err := client.CreateCompletionStream(ctx, req)
+	if err != nil {
+		fmt.Errorf("CreateCompletionStream returned error: %v", err)
+	}
+	defer resp.Close()
+
+	counter := 0
+	for {
+		data, err := resp.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Errorf("Stream error: %v", err)
+		} else {
+			counter++
+			fmt.Print(data.Choices[0].Text)
+
+		}
+	}
+	if counter == 0 {
+		fmt.Errorf("Stream did not return any responses")
+	}
+	fmt.Println("")
+}
+
+type NullWriter int
+
+func (NullWriter) Write([]byte) (int, error) { return 0, nil }
+
+func main() {
+	log.SetOutput(new(NullWriter))
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		panic("Missing API KEY")
+	}
+
+	ctx := context.Background()
+	client := openai.NewClient(apiKey)
+	rootCmd := &cobra.Command{
+		Use:   "chatgpt",
+		Short: "Chat with ChatGPT in console.",
+		Run: func(cmd *cobra.Command, args []string) {
+			scanner := bufio.NewScanner(os.Stdin)
+			quit := false
+
+			for !quit {
+				fmt.Print("Input your question (type `quit` to exit): ")
+
+				if !scanner.Scan() {
+					break
+				}
+
+				question := scanner.Text()
+				questionParam := validateQuestion(question)
+				switch questionParam {
+				case "quit":
+					quit = true
+				case "":
+					continue
+
+				default:
+					GetResponse(client, ctx, questionParam)
+				}
+			}
+		},
+	}
+
+	log.Fatal(rootCmd.Execute())
+}
+
+func validateQuestion(question string) string {
+	quest := strings.Trim(question, " ")
+	keywords := []string{"", "loop", "break", "continue", "cls", "exit", "block"}
+	for _, x := range keywords {
+		if quest == x {
+			return ""
+		}
+	}
+	return quest
+}
